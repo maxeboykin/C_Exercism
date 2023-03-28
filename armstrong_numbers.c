08980a6852e57b1d4a292ee57b37770ff33c6e23
Max Boykin <maxericboykin@gmail.com>
2023-03-27 23:23:52 -0400

    ran by commenting out armstrong

--- armstrong_numbers.c
+++ armstrong_numbers.c
@@ -5,25 +5,25 @@
 #include "armstrong_numbers.h"
 #include <math.h>
 
-bool is_armstrong_number(int candidate){
-
-    if (candidate < 10)
-        return true;
-
-    int digit_count = log10(candidate) + 1;
-
-    int num = candidate;
-    int pow_total = 0;
-    while(num > 0)
-    {
-        int pow_temp = num % 10;
-        int pow_temp_total = 1;
-        for (int i = 1; i < digit_count; i+=1){
-            pow_temp_total *= pow_temp;
-        }
-        pow_total += pow_temp_total;
-        num /= 10;
-    }
-
-    return pow_total == candidate;
-}
+//bool is_armstrong_number(int candidate){
+//
+//    if (candidate < 10)
+//        return true;
+//
+//    int digit_count = log10(candidate) + 1;
+//
+//    int num = candidate;
+//    int pow_total = 0;
+//    while(num > 0)
+//    {
+//        int pow_temp = num % 10;
+//        int pow_temp_total = 1;
+//        for (int i = 1; i < digit_count; i+=1){
+//            pow_temp_total *= pow_temp;
+//        }
+//        pow_total += pow_temp_total;
+//        num /= 10;
+//    }
+//
+//    return pow_total == candidate;
+//}
--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 int main() {
 
     is_isogram("alphbetuy");
-    is_armstrong_number(152);
+   // is_armstrong_number(152);
     compute("ABC", "ABC");
     difference_of_squares(50);
     return 0;
